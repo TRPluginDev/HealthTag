71e30ec15e907a84b0f9957748b87148f4a0d1c1
ertenysf <ertenysf@hotmail.com>
2017-01-14 18:22:13 +0200

    Update CanT.php

--- src/ertenysf/CanT.php
+++ src/ertenysf/CanT.php
@@ -7,18 +7,15 @@ use pocketmine\scheduler\PluginTask;
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\Plugin;
-		public function onRun($tick){
-	  foreach($this->getOwner()->getServer()->getOnlinePlayers() as $o){
-		  // - getNametag
-        $cfg = new Config($this->getDataFolder()."config.yml", Con
 use pocketmine\event\Listener;
 					
-	class CanT extends PluginTask {
-	fig::YAML);
+class CanT extends PluginTask {
+ public function onRun($tick){
+	   foreach($this->getOwner()->getServer()->getOnlinePlayers() as $o){
 	    $player = $o;
+		        $cfg = new Config($this->getDataFolder()."config.yml", Con
 		$tag = strreplace("{oyuncu}", $o->getName(), $cfg->ge("Tag"));
 	    $o->setNameTag($tag . "\n   " . ($o->getHealth() / $o->getMaxHealth() * 20).$cfg->get("Sembol"));
-	  
 	  }
      }
 	}
