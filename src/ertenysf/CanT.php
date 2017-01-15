71834be56fd9c5f36c4e8b15401f47b973246fb7
TRPluginDev <ertenysf01@gmail.com>
2017-01-15 13:53:42 +0300

    Update CanT.php

--- src/ertenysf/CanT.php
+++ src/ertenysf/CanT.php
@@ -13,7 +13,7 @@ class CanT extends PluginTask {
  public function onRun($tick){
 	   foreach($this->getOwner()->getServer()->getOnlinePlayers() as $o){
 	    $player = $o;
-		        $cfg = new Config($this->getDataFolder()."config.yml", Con
+        $cfg = new Config($this->getDataFolder()."config.yml", Config::YAML);
 		$tag = str_replace("{oyuncu}", $o->getName(), $cfg->get("Tag"));
 	    $o->setNameTag($tag . "\n   " . ($o->getHealth() / $o->getMaxHealth() * 20).$cfg->get("Sembol"));
 	  }
